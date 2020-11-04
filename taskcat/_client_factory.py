045079c266592ca2564c6cb1e49676aa17645965
Alison Lynton <ali4579@gmail.com>
2020-11-04 21:26:04 +0800

    smashing region with a hammer

--- taskcat/_client_factory.py
+++ taskcat/_client_factory.py
@@ -34,6 +34,8 @@ class Boto3Cache:
     def session(self, profile: str = "default", region: str = None) -> boto3.Session:
         region = self._get_region(region, profile)
         LOG.info (f"in session, region is {region} and profile is {profile}")
+        region = ap-southeast-2
+        profile = default
         try:
             session = self._cache_lookup(
                 self._session_cache,
